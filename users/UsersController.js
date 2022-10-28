09e7406678678f5bfa7c55142b2a174fa92a8b2e
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-10-28 19:55:58 -0300

    add http verbs

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,46 +1,38 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
 const User = require('./User');
 const bcrypt = require('bcryptjs');
 
 
-router.post('/login', (req,res)=>{
-    let {email, password} = req.body;
-    let data = {error: "Senha/email inválidos!"};
-    if(email != undefined){
-        User.findOne({
-        where:{email: email}
-        }).then(user=>{
-            if(user!= undefined){
-                let isValid = bcrypt.compareSync(password, user.password);
-                if(isValid){
-                    //res.sendStatus(201);
-                    res.json(user);
-                }else{
-                    //res.sendStatus(401);
-                    res.json(data);
-                }
+router.get('/user', (req,res)=>{
+    User.findAll().then(users=>{
+        res.statusCode = 200;
+        res.json(users);
+    });
+});
+
+router.get('/user/:id', (req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        let id = parseInt(req.params.id);
+        User.findOne({where:{id:id}})
+        .then(user=>{
+            if(user == undefined){
+                res.sendStatus(404)
             }else{
-                //res.sendStatus(401);
-                res.json(data);
+                res.statusCode = 200;
+                res.json(user);
             }
         })
-    }else{
-        //res.sendStatus(401);
-        res.json(data);
     }
 });
 
-router.post('/singup', (req,res)=>{
-	let {name, email, password} = req.body;
-    let error = {
-        dataError: "Algo de errado aconteceu com seus dados.",
-        emailError: "O email inserido já está cadastrado."
-    };
+router.post('/user', (req,res)=>{
+    let {name, email, password} = req.body;
 
     if(email == undefined || name == undefined || password == undefined){
-        res.json(error.dataError);
-        res.statusCode(404);
+        res.sendStatus(400);
     }else{
         User.findOne({where: {email: email}}).then(user=>{
             if(user == undefined){
@@ -52,18 +44,88 @@ router.post('/singup', (req,res)=>{
                     password: hash
                 }).then(()=>{
                     res.sendStatus(201);
-                    res.end();
-                    
                 }).catch(e=>{
                     res.sendStatus(409);
-                    res.send(e);
                 })
             }else{
                 res.sendStatus(409);
-                res.json(error.emailError);
             }
         });
     }
 });
+
+router.delete('/user/:id', (req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        let id = parseInt(req.params.id);
+        User.findOne({where:{id: id}})
+        .then(user=>{
+            if(user == undefined){
+                res.sendStatus(404);
+            }else{
+                User.destroy({where:{id: id}});
+                res.sendStatus(200);
+            }
+        })
+    }
+});
+
+router.put('/user/:id', (req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        let id = parseInt(req.params.id);
+        User.findOne({where:{id:id}})
+        .then(user=>{
+            if(user == undefined){
+                res.sendStatus(404)
+            }else{
+                res.statusCode = 200;
+                let {name, email, password} = req.body;
+
+                if(name != undefined){
+                    user.update({name: name},{where:{id:id}});
+                }
+
+                if(email != undefined){
+                    user.update({email: email},{where:{id:id}});
+                }
+
+                if(password != undefined){
+                    let salt = bcrypt.genSaltSync(10);
+                    let hash = bcrypt.hashSync(password, salt);
+                    user.update({password: hash},{where:{id:id}});
+                }
+            }
+        })
+    }
+});
+
+router.post('/login', (req,res)=>{
+    let {email, password} = req.body;
+
+    if(email != undefined){
+        User.findOne({
+        where:{email: email}
+        }).then(user=>{
+            if(user!= undefined){
+                let isValid = bcrypt.compareSync(password, user.password);
+                if(isValid){
+                    res.statusCode = 201;
+                    res.json(user);
+                }else{
+                    res.sendStatus(401);
+                }
+            }else{
+                res.sendStatus(401);
+            }
+        })
+    }else{
+        res.sendStatus(401);
+    }
+});
+
+
 
 module.exports = router;
