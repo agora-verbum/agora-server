79437428ee5f0ac78a3b14172a0a9eab2ea66857
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-11-15 00:53:14 -0300

    updating packages and index

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const port = 80;
 const connection = require('./database/database.js');
 const bodyParser = require('body-parser');
 const UsersController = require('./users/UsersController.js');
-const User = require('./users/User.js');
+const PostsController = require('./posts/PostsController.js');
 const CategoriesController = require('./categories/CategoriesController.js');
-const Category = require('./categories/Category.js');
 const cors = require('cors');
 const e = require('express');
 
@@ -16,22 +15,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-connection
-    .authenticate()
-
-    .then(() => {
-        console.log("Successful connection.");
-    })
-
-    .catch((errorMsg)=>{
-        console.log(errorMsg);
-    });
-
 
 
 app.use('/', UsersController);
+
+app.use('/', PostsController);
 app.use('/', CategoriesController);
 
 
 
-app.listen(80, ()=>console.log(`Server runing in ${port} port`));
+app.listen(3000, ()=>console.log(`Server runing in ${port} port`));
